d8e1e456848f1bd9c6a27611db111d134e60a6ed
tikuta <tikuta@localhost>
2019-05-14 21:00:44 +0900

    master

--- sandbox/fib.cc
+++ sandbox/fib.cc
@@ -1,6 +1,9 @@
 int fib(int n) {
-  if (n <= 1) {
-	return 1;
+  int a = 1, b = 1;
+  for (int i = 0; i < n; ++i) {
+	int c = a + b;
+	a = b;
+	b = c;
   }
-  return fib(n - 1) + fib(n - 2);
+  return a;
 }
