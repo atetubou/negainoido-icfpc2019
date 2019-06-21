d6da5c0241efa310bb5b03ff24f529586cd75751
cympfh <cympfh@gmail.com>
2019-06-22 06:22:02 +0900

    test

--- express_webapp/static/stat.js
+++ express_webapp/static/stat.js
@@ -1,39 +1,37 @@
 function draw_data(data) {
 
-  $("#content-table").tabulator("setData", data);
-
-  // var rows = [];
-  // for (var i = 0; i < data.length; ++i) {
-  //   rows[i] = data[i].best;
-  // }
-  //
-  // var table = new Tabulator("#content", {
-  //   data: rows,           //load row data from array
-  //   layout:"fitColumns",      //fit columns to width of table
-  //   responsiveLayout:"hide",  //hide columns that dont fit on the table
-  //   tooltips:true,            //show tool tips on cells
-  //   addRowPos:"top",          //when adding a new row, add it to the top of the table
-  //   history:true,             //allow undo and redo actions on the table
-  //   pagination:"local",       //paginate the data
-  //   paginationSize:7,         //allow 7 rows per page of data
-  //   movableColumns:true,      //allow column order to be changed
-  //   resizableRows:true,       //allow row order to be changed
-  //   initialSort:[             //set the initial sort order of the data
-  //     {column:"task_id", dir:"asc"},
-  //   ],
-  //   columns: [
-  //     {title: "best",
-  //       columns: [
-  //         {title:"id", field:"id"},
-  //         {title:"solver", field:"solver"},
-  //         {title:"task_id", field:"task_id"},
-  //         {title:"score", field:"score"},
-  //         {title:"created", field:"created"},
-  //         {title:"updatedAt", field:"updatedAt"},
-  //       ]
-  //     }
-  //   ]
-  // });
+  var rows = [];
+  for (var i = 0; i < data.length; ++i) {
+    rows[i] = data[i].best;
+  }
+  console.log(rows);
+  var table = new Tabulator("#content", {
+    data: rows,           //load row data from array
+    layout:"fitColumns",      //fit columns to width of table
+    responsiveLayout:"hide",  //hide columns that dont fit on the table
+    tooltips:true,            //show tool tips on cells
+    addRowPos:"top",          //when adding a new row, add it to the top of the table
+    history:true,             //allow undo and redo actions on the table
+    pagination:"local",       //paginate the data
+    paginationSize:7,         //allow 7 rows per page of data
+    movableColumns:true,      //allow column order to be changed
+    resizableRows:true,       //allow row order to be changed
+    initialSort:[             //set the initial sort order of the data
+      {column:"task_id", dir:"asc"},
+    ],
+    columns: [
+      {title: "best",
+        columns: [
+          {title:"id", field:"id"},
+          {title:"solver", field:"solver"},
+          {title:"task_id", field:"task_id"},
+          {title:"score", field:"score"},
+          {title:"created", field:"created"},
+          {title:"updatedAt", field:"updatedAt"},
+        ]
+      }
+    ]
+  });
 }
 
 function load_data() {
