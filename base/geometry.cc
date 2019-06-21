15f21b619a84dc9b0914b5ccede35e9c4baad163
Keiichi Watanabe <udon.watanabe@gmail.com>
2019-06-22 00:48:47 +0900

    Implement reachable()

---
+++ base/geometry.cc
@@ -0,0 +1,102 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+
+#include <cassert>
+
+#include "base/geometry.h"
+
+using namespace std;
+
+Point crosspoint(const Line &l, const Line &m);
+
+
+const double EPS = 1e-8;
+const double INF = 1e12;
+
+
+bool operator < (const Point& a, const Point& b) {
+  return real(a) != real(b) ? real(a) < real(b) : imag(a) < imag(b);
+}
+
+
+double cross(const Point& a, const Point& b) {
+  return imag(conj(a)*b);
+}
+double dot(const Point& a, const Point& b) {
+  return real(conj(a)*b);
+}
+
+
+typedef vector<Point> G;
+
+struct C {
+  Point p; double r;
+  C(const Point &p, double r) : p(p), r(r) { }
+};
+
+
+int ccw(Point a, Point b, Point c) {
+  b -= a; c -= a;
+  if (cross(b, c) > 0)   return +1;       // counter clockwise
+  if (cross(b, c) < 0)   return -1;       // clockwise
+  if (dot(b, c) < 0)     return +2;       // c--a--b on line
+  if (norm(b) < norm(c)) return -2;       // a--b--c on line
+  return 0;
+}
+
+bool intersectLL(const Line &l, const Line &m) {
+  return abs(cross(l[1]-l[0], m[1]-m[0])) > EPS || // non-parallel
+         abs(cross(l[1]-l[0], m[0]-l[0])) < EPS;   // same line
+}
+bool intersectLS(const Line &l, const Line &s) {
+  return cross(l[1]-l[0], s[0]-l[0])*       // s[0] is left of l
+         cross(l[1]-l[0], s[1]-l[0]) < EPS; // s[1] is right of l
+}
+bool intersectLP(const Line &l, const Point &p) {
+  return abs(cross(l[1]-p, l[0]-p)) < EPS;
+}
+bool intersectSS(const Line &s, const Line &t) {
+  return ccw(s[0],s[1],t[0])*ccw(s[0],s[1],t[1]) < 0 &&
+         ccw(t[0],t[1],s[0])*ccw(t[0],t[1],s[1]) < 0;
+}
+bool intersectSP(const Line &s, const Point &p) {
+  return abs(s[0]-p)+abs(s[1]-p)-abs(s[1]-s[0]) < EPS; // triangle inequality
+}
+
+Point projection(const Line &l, const Point &p) {
+  double t = dot(p-l[0], l[0]-l[1]) / norm(l[0]-l[1]);
+  return l[0] + t*(l[0]-l[1]);
+}
+Point reflection(const Line &l, const Point &p) {
+  return p + 2.0 * (projection(l, p) - p);
+}
+double distanceLP(const Line &l, const Point &p) {
+  return abs(p - projection(l, p));
+}
+double distanceLL(const Line &l, const Line &m) {
+  return intersectLL(l, m) ? 0 : distanceLP(l, m[0]);
+}
+double distanceLS(const Line &l, const Line &s) {
+  if (intersectLS(l, s)) return 0;
+  return min(distanceLP(l, s[0]), distanceLP(l, s[1]));
+}
+double distanceSP(const Line &s, const Point &p) {
+  const Point r = projection(s, p);
+  if (intersectSP(s, r)) return abs(r - p);
+  return min(abs(s[0] - p), abs(s[1] - p));
+}
+double distanceSS(const Line &s, const Line &t) {
+  if (intersectSS(s, t)) return 0;
+  return min(min(distanceSP(s, t[0]), distanceSP(s, t[1])),
+             min(distanceSP(t, s[0]), distanceSP(t, s[1])));
+}
+
+Point crosspoint(const Line &l, const Line &m) {
+  double A = cross(l[1] - l[0], m[1] - m[0]);
+  double B = cross(l[1] - l[0], l[1] - m[0]);
+  if (abs(A) < EPS && abs(B) < EPS) return m[0]; // same line
+  if (abs(A) < EPS) assert(false); // !!!PRECONDITION NOT SATISFIED!!!
+  return m[0] + B / A * (m[1] - m[0]);
+}
--- base/main.cc
+++ base/main.cc
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <cstdio>
 #include "base/ai.h"
 
 int main() {
   AI ai;
-  std::cout << static_cast<int>(ai.get_dir()) << std::endl;
-  ai.turn_CW();
-  std::cout << static_cast<int>(ai.get_dir()) << std::endl;
+  //std::cout << static_cast<int>(ai.get_dir()) << std::endl;
+  //ai.turn_CW();
+  //std::cout << static_cast<int>(ai.get_dir()) << std::endl;
+
+  printf("%d %d\n", ai.get_pos().first,ai.get_pos().second);
+
+  for (int i = 0; i < ai.h; i++) {
+    for (int j = 0; j < ai.w; j++) {
+      printf("%d", ai.reachable(std::make_pair(i, j)));
+    }
+    printf("\n");
+  }
 }
