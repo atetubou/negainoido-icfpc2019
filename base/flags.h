7f01611d353efa58bea66e68ef7c338436791f06
Takuto Ikuta <atetubou@gmail.com>
2019-06-21 20:54:06 +0900

    add a base (#64)

---
+++ base/flags.h
@@ -0,0 +1,8 @@
+#ifndef BASE_FLAGS_H_
+#define BASE_FLAGS_H_
+
+#include "gflags/gflags.h"
+
+DEFINE_string(problem_filename, "-", "filename for problem file e.g. part-1-initial/prob-001.desc");
+
+#endif  // BASE_FLAGS_H_
