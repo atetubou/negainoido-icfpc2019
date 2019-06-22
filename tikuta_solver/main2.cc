9a3f5dc7cb622a0c7800c3a19a35224f0e96e27f
Takuto Ikuta <atetubou@gmail.com>
2019-06-22 18:49:37 +0900

    bugver2 (#140)
    * git squash commit for main2.
    
    26e688b7ae7193feb757789f7a70b636aff6d441
    git squash commit for main2.
    
    aed22f3cd87b881e87e892a06dd3c67e3e511dca
    git squash commit for tikuta2.
    
    b134e3384d124e6818ab1fe08951e4dfc8750e78
    wip
    
    aaed1b097b5560cea1e889663f39f275cae59226
    wip
    
    c4d8c5d087a593730a63b8aee9a9b89d55bd54c8
    difier
    
    5c60dcc97bfb8533e00c491bba7bf222c749753a
    wip
    
    966ce1e1009fc46f8e5146dcdced9d019725c183
    wip
    
    02507166b7dbd8f8f59f8b77d9ec65cec38bbee7
    wip
    
    0c8133a076f776ea0a51da44e0cb7a0d782ccd2b
    wip
    
    7ce4eda1c7a1622a571aa4a9eb97315616a12b0b
    wip
    
    5c568e30c2ab27a9e0d7903c2a5c0404f3a6beae
    wip
    
    930212ea51e3767f6e429f0bc53ae03e73459489
    wip
    
    12b8ceb0f3642d03ecdeaf0d7e93a57c7fc8c5a1
    main2
    
    df071f5ca5ef63df8c13a4ca2d4b332e688a7c2f
    main2
    
    * bug

---
+++ tikuta_solver/main2.cc
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+
+#include "absl/strings/str_join.h"
+
+#include "base/ai.h"
+#include "base/graph.h"
+#include "tailed/LKH3_wrapper.h"
+
+DEFINE_string(LKH3path, "./tailed/LKH", "");
+
+using pos = std::pair<int, int>;
+
+std::vector<std::vector<pos>> get_groups(const AI& ai,
+					 const std::vector<pos>& tsp_tours) {
+  int w = ai.board.size();
+  int h = ai.board[0].size();
+
+  std::vector<std::vector<bool>> visited(w, std::vector<bool>(h));
+  std::vector<std::vector<pos>> groups(tsp_tours.size());
+
+  std::queue<std::pair<int, pos>> q;
+  for (auto i = 0u; i < tsp_tours.size(); ++i) {
+    q.push(std::make_pair(i, tsp_tours[i]));
+  }
+
+  int dx[] = {0, 0, 1, -1};
+  int dy[] = {1, -1, 0, 0};
+
+  while (!q.empty()) {
+    auto cur = q.front();
+    q.pop();
+    auto curpos = cur.second;
+    if (visited[curpos.first][curpos.second]) {
+      continue;
+    }
+    visited[curpos.first][curpos.second] = true;
+    groups[cur.first].push_back(curpos);
+
+    for (int i = 0; i < 4; ++i) {
+      int nx = curpos.first + dx[i];
+      int ny = curpos.second + dy[i];
+      if (nx < 0 || ny < 0 || nx >= w || ny >= h ||
+	  ai.board[nx][ny] == '#' || visited[nx][ny]) {
+	continue;
+      }
+      q.push({cur.first, {nx, ny}});
+    }
+  }
+
+  return groups;
+}
+
+int main(int argc, char *argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+
+  // AI's constructor accepts a input file from stdin.
+  AI ai;
+
+  const AI init_ai = ai;
+
+  auto tsp_tours = SolveShrinkedTSP(ai.board, 100, FLAGS_LKH3path);
+
+  auto groups = get_groups(ai, tsp_tours);
+
+  std::vector<pos> order;
+  for (const auto& group : groups) {
+    for (const auto& p : group) {
+      order.push_back(p);
+    }
+  }
+
+  GridGraph gridg(ai.board);
+
+  for (auto i = 0u; i + 1 < order.size(); ++i) {
+    const auto& s = order[i];
+    const auto& g = order[i+1];
+    std::vector<pos> path;
+    gridg.shortest_path(s.first, s.second,
+			g.first, g.second, path);
+    auto actions = GridGraph::path_to_actions(path);
+    for (auto c : actions) {
+      std::cout << direction_to_char(c);
+    }
+  }
+  std::cout << std::endl;
+}
