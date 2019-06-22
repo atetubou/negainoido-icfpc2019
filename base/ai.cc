c38429a4372b88b9e2ecfc7dd704005e07dbcc63
Keiichi Watanabe <udon.watanabe@gmail.com>
2019-06-23 04:43:09 +0900

    ai.cc: Use vector for worker

--- base/ai.cc
+++ base/ai.cc
@@ -20,6 +20,7 @@ char direction_to_char(Direction d) {
   }
 
   LOG(FATAL) << "Invalid direction " << static_cast<int>(d);
+  return '!';
 }
 
 // relative p when Right => ??? when dir
@@ -70,7 +71,7 @@ bool AI::valid_pos(Position target) {
   return 0 <= x && x < height && 0 <= y && y < width;
 }
 
-bool AI::reachable(Position target) {
+bool AI::reachable(Position target, const int id) {
   int x = target.first;
   int y = target.second;
   if (!valid_pos(target))
@@ -79,7 +80,7 @@ bool AI::reachable(Position target) {
   if (board[x][y] == '#')
     return false;
 
-  auto worker_pos = get_pos();
+  auto worker_pos = get_pos(id);
   int wx = worker_pos.first;
   int wy = worker_pos.second;
   for (int cx = std::min(x, wx); cx <= std::max(x, wx); cx++) {
@@ -127,12 +128,13 @@ void AI::initialize() {
   filled.resize(height);
   for (int i = 0; i < height; ++i) filled[i].resize(width);
 
-  worker = Worker(worker_pos);
+  workers.clear();
+  workers.push_back(Worker(worker_pos));
 
-  for (auto&p : worker.manipulator_range) {
-    auto x = worker.current_pos.first + p.first;
-    auto y = worker.current_pos.second + p.second;
-    fill_cell(std::make_pair(x, y));
+  for (auto &p : workers[0].manipulator_range) {
+    auto x = workers[0].current_pos.first + p.first;
+    auto y = workers[0].current_pos.second + p.second;
+    fill_cell(std::make_pair(x, y), 0);
   }
 }
 
@@ -150,10 +152,10 @@ AI::AI(const std::string filename) {
   initialize();
 }
 
-bool AI::fill_cell(Position pos) {
+bool AI::fill_cell(Position pos, const int id) {
   int x = pos.first;
   int y = pos.second;
-  if (!reachable(pos)) {
+  if (!reachable(pos, id)) {
     return false;
   }
 
@@ -161,19 +163,19 @@ bool AI::fill_cell(Position pos) {
     filled_count++;
   }
   filled[x][y] = true;
-  if (pos != worker.current_pos)
+  if (pos != workers[id].current_pos)
     return true;
 
   // Pick up a booster if pos == worker's position
   switch(board[x][y]) {
     case 'B':
-      worker.count_extension += 1;
+      workers[id].count_extension += 1;
       break;
     case 'F':
-      worker.count_fast += 1;
+      workers[id].count_fast += 1;
       break;
     case 'L':
-      worker.count_drill += 1;
+      workers[id].count_drill += 1;
       break;
     default:
       break;
@@ -190,31 +192,31 @@ int AI::get_block_count() const { return block_count; }
 
 int AI::get_time() { return current_time; }
 
-Position AI::get_pos() const { return worker.current_pos; }
+Position AI::get_pos(const int id) const { return workers[id].current_pos; }
 
-Direction AI::get_dir() { return worker.current_dir; }
+Direction AI::get_dir(const int id) { return workers[id].current_dir; }
 
 int AI::get_filled_count() { return filled_count; }
 
-int AI::get_count_fast() { return worker.count_fast; }
-int AI::get_count_drill() { return worker.count_drill; }
-int AI::get_count_extension() { return worker.count_extension; }
+int AI::get_count_fast(const int id) { return workers[id].count_fast; }
+int AI::get_count_drill(const int id) { return workers[id].count_drill; }
+int AI::get_count_extension(const int id) { return workers[id].count_extension; }
 
-int AI::get_duration_fast() { return worker.duration_fast; }
-int AI::get_duration_drill() { return worker.duration_drill; }
+int AI::get_duration_fast(const int id) { return workers[id].duration_fast; }
+int AI::get_duration_drill(const int id) { return workers[id].duration_drill; }
 
-Position AI::get_neighbor(const Direction &dir) {
+Position AI::get_neighbor(const Direction &dir, const int id) {
   const int dx[] = {0,1, 0,-1};
   const int dy[] = {1,0,-1, 0};
   int idx = static_cast<int>(dir);
 
-  return Position(worker.current_pos.first + dx[idx],
-                  worker.current_pos.second + dy[idx]);
+  return Position(workers[id].current_pos.first + dx[idx],
+                  workers[id].current_pos.second + dy[idx]);
 }
 
-void AI::turn_CW() {
-  worker.current_dir =
-    static_cast<Direction>( ( static_cast<int>(worker.current_dir) + 1 ) % 4 );
+void AI::turn_CW(const int id) {
+  workers[id].current_dir =
+    static_cast<Direction>( ( static_cast<int>(workers[id].current_dir) + 1 ) % 4 );
   executed_cmds.push_back("E");
   for(auto p: get_absolute_manipulator_positions()) {
     fill_cell(p);
@@ -223,9 +225,9 @@ void AI::turn_CW() {
   next_turn();
 }
 
-void AI::turn_CCW() {
-  worker.current_dir =
-    static_cast<Direction>( ( static_cast<int>(worker.current_dir) - 1 ) % 4 );
+void AI::turn_CCW(const int id) {
+  workers[id].current_dir =
+    static_cast<Direction>( ( static_cast<int>(workers[id].current_dir) - 1 ) % 4 );
   executed_cmds.push_back("Q");
   for(auto p: get_absolute_manipulator_positions()) {
     fill_cell(p);
@@ -234,13 +236,13 @@ void AI::turn_CCW() {
   next_turn();
 }
 
-std::vector<Position> AI::get_absolute_manipulator_positions() {
+std::vector<Position> AI::get_absolute_manipulator_positions(const int id) {
 
-  Position self = get_pos();
+  Position self = get_pos(id);
   std::vector<Position> ret;
 
-  for (Position&p : worker.manipulator_range) {
-    Position mani = rotate(p, get_dir());
+  for (Position&p : workers[id].manipulator_range) {
+    Position mani = rotate(p, get_dir(id));
     mani.first += self.first;
     mani.second += self.second;
     ret.push_back(mani);
@@ -250,56 +252,59 @@ std::vector<Position> AI::get_absolute_manipulator_positions() {
 
 void AI::next_turn() {
   current_time++;
-  if (worker.duration_drill > 0)
-    worker.duration_drill--;
-  if (worker.duration_fast > 0)
-    worker.duration_fast--;
+
+  for (auto &worker: workers) {
+    if (worker.duration_drill > 0)
+      worker.duration_drill--;
+    if (worker.duration_fast > 0)
+      worker.duration_fast--;
+  }
 }
 
-bool AI::try_move(const Direction &dir) {
-  Position next_pos = get_neighbor(dir);
+bool AI::try_move(const Direction &dir, const int id) {
+  Position next_pos = get_neighbor(dir, id);
 
   // Check validity
   if (!valid_pos(next_pos))
     return false;
 
-  if (board[next_pos.first][next_pos.second] == '#' && worker.duration_drill == 0)
+  if (board[next_pos.first][next_pos.second] == '#' && workers[id].duration_drill == 0)
     return false;
 
   return true;
 }
 
-bool AI::move_body(const Direction &dir) {
+bool AI::move_body(const Direction &dir, const int id) {
   // Checks if this is a valid move
-  if(!try_move(dir))
+  if(!try_move(dir, id))
     return false;
 
   auto pos = get_neighbor(dir);
   // Move
-  worker.current_pos = pos;
+  workers[id].current_pos = pos;
 
   // Check drill
   if (board[pos.first][pos.second] == '#' &&
-      0 < worker.duration_drill) {
+      0 < workers[id].duration_drill) {
     board[pos.first][pos.second] = '.';
     block_count--;
   }
 
   // Pick up items
-  for(auto p: get_absolute_manipulator_positions()) {
-    fill_cell(p);
+  for(auto p: get_absolute_manipulator_positions(id)) {
+    fill_cell(p, id);
   }
 
   return true;
 }
 
-bool AI::move(const Direction &dir) {
-  if (!move_body(dir))
+bool AI::move(const Direction &dir, const int id) {
+  if (!move_body(dir, id))
     return false;
 
   // When FAST booster is enabled, move twice
-  if (worker.duration_fast > 0) {
-    move_body(dir);
+  if (workers[id].duration_fast > 0) {
+    move_body(dir, id);
   }
 
   // Push commandx
@@ -325,42 +330,43 @@ void AI::print_commands() {
   std::cout << std::endl;
 }
 
-bool AI::use_fast_wheel() {
-  if (worker.count_fast == 0)
+bool AI::use_fast_wheel(const int id) {
+  if (workers[id].count_fast == 0)
     return false;
 
-  worker.duration_fast = Worker::DURATION_FAST_MAX;
-  worker.count_fast--;
+  workers[id].duration_fast = Worker::DURATION_FAST_MAX;
+  workers[id].count_fast--;
   executed_cmds.push_back("F");
 
   next_turn();
   return true;
 }
 
-bool AI::use_drill() {
-  if (worker.count_drill == 0)
+bool AI::use_drill(const int id) {
+  if (workers[id].count_drill == 0)
     return false;
 
-  worker.duration_drill = Worker::DURATION_DRILL_MAX;
-  worker.count_drill--;
+  workers[id].duration_drill = Worker::DURATION_DRILL_MAX;
+  workers[id].count_drill--;
   executed_cmds.push_back("L");
 
   next_turn();
   return true;
 }
 
-bool AI::use_extension(const int dx, const int dy) {
-  if (worker.count_extension == 0)
+bool AI::use_extension(const int dx, const int dy, const int id) {
+  if (workers[id].count_extension == 0)
     return false;
 
-  for (auto m: worker.manipulator_range) {
+  for (auto m: workers[id].manipulator_range) {
     if (Position(dx, dy) == m) {
       return false;
     }
   }
 
   bool can_use = false;
-  for(auto m: worker.manipulator_range) {
+
+  for(auto m: workers[id].manipulator_range) {
     if (std::abs(m.first - dx) + std::abs(m.second - dy) == 1) {
       can_use = true;
     }
@@ -369,8 +375,8 @@ bool AI::use_extension(const int dx, const int dy) {
   if(!can_use)
     return false;
 
-  worker.manipulator_range.push_back( rotate_reverse({dx, dy}, get_dir()) );
-  worker.count_extension--;
+  workers[id].manipulator_range.push_back( rotate_reverse({dx, dy}, get_dir(id)) );
+  workers[id].count_extension--;
 
   // Convert coordinate: (dx, dy) -> (dy, -dx)
   auto cmd = "B(" + std::to_string(dy) + "," + std::to_string(-dx) + ")";
@@ -383,14 +389,24 @@ bool AI::use_extension(const int dx, const int dy) {
 
 void AI::dump_state() {
   std::cerr << "time: " << current_time << ", filled_count: " << filled_count << std::endl;
-  std::cerr << "pos: (" << worker.current_pos.first << ", " << worker.current_pos.second << ")" << std::endl;
-  std::cerr << "duration: drill=" << worker.duration_drill << ", fast: " << worker.duration_fast << std::endl;
-  auto p = get_pos();
+
+  std::set<Position> ws;
+
+  for (uint32_t i = 0; i < workers.size(); i++) {
+    auto p = get_pos(i);
+    ws.insert(p);
+
+    std::cerr << "pos[" << i << "]: ("
+              << p.first << ", " << p.second << ")"
+              << std::endl;
+    std::cerr << "duration: drill=" << workers[i].duration_drill
+              << ", fast: " << workers[i].duration_fast << std::endl;
+  }
 
   for(int i = 0; i < height; ++i) {
     for(int j = 0; j < width; ++j) {
-      if(i == p.first && j == p.second) {
-        std::cerr << "@";
+      if (ws.find(std::make_pair(i, j)) != ws.end()) {
+        std::cerr << "W";
       } else if(filled[i][j]) {
         std::cerr << "V";
       } else {
--- base/ai.h
+++ base/ai.h
@@ -57,13 +57,14 @@ class AI {
   int filled_count = 0;
   int block_count = 0;
 
-  bool fill_cell(Position);
+  bool fill_cell(Position, const int id = 0);
 
-  Worker worker;
+  int expect_worker_id = 0;   // Rotate from 0 to (workers.size()-1)
+  std::vector<Worker> workers;
 
   void next_turn();
   bool valid_pos(Position);
-  bool move_body(const Direction &dir);
+  bool move_body(const Direction &dir, const int id = 0);
 
 
   void initialize();
@@ -85,44 +86,45 @@ public:
   // Gets current time
   int get_time();
   // Gets the current position
-  Position get_pos() const;
+  Position get_pos(const int id = 0) const;
+
   // Gets the current direction
-  Direction get_dir();
+  Direction get_dir(const int id = 0);
   // Gets the current direction
   int get_filled_count();
 
   // Gets the number of available boosters
-  int get_count_fast();
-  int get_count_drill();
-  int get_count_extension();
+  int get_count_fast(const int id = 0);
+  int get_count_drill(const int id = 0);
+  int get_count_extension(const int id = 0);
 
   // Gets the unit time until each tool is consumed.
   // Returns 0 if the tool is not used now.
-  int get_duration_fast();
-  int get_duration_drill();
+  int get_duration_fast(const int id = 0);
+  int get_duration_drill(const int id = 0);
 
   // Returns the position that is next to the current position.
-  Position get_neighbor(const Direction &dir);
+  Position get_neighbor(const Direction &dir, const int id = 0);
 
   // Gets the all (possible) positions of manipulators.
   // It may be in invalid postion. (e.g. out of the map, unreachable postion)
-  std::vector<Position> get_absolute_manipulator_positions();
+  std::vector<Position> get_absolute_manipulator_positions(const int id = 0);
 
   // Checks if pos is 'reachable' from the current position.
-  bool reachable(Position pos);
+  bool reachable(Position pos, const int id = 0);
 
   // Checks if the robot can move to the direction.
   // This doesn't change any internal state.
-  bool try_move(const Direction &dir);
+  bool try_move(const Direction &dir, const int id = 0);
 
   // Executes a command and updates the internal state.
   // If it's a invalid move, it returns false without changing internal states.
-  bool move(const Direction &dir);
-  bool use_extension(const int dx, const int dy);
-  bool use_fast_wheel();
-  bool use_drill();
-  void turn_CW();  // Turn commands always succeed
-  void turn_CCW();
+  bool move(const Direction &dir, const int id = 0);
+  bool use_extension(const int dx, const int dy, const int id = 0);
+  bool use_fast_wheel(const int id = 0);
+  bool use_drill(const int id = 0);
+  void turn_CW(const int id = 0);  // Turn commands always succeed
+  void turn_CCW(const int id = 0);
 
   // Checks if get_filled_count() == Height * Width
   bool is_finished();
