c7638154cec8c6b5f796a187607d35dde58bbf90
Keiichi Watanabe <udon.watanabe@gmail.com>
2019-06-23 04:19:59 +0900

    Move input_formatter.py (#171)

--- puzzle_solver/input_formatter.py
+++ puzzle_solver/input_formatter.py
@@ -28,10 +28,10 @@ def output_puzzle(basename, inp):
 
     assert(len(nums) == 11)
 
-    fname = basename + ".puzzle.txt"
+    fname = basename + ".txt"
 
     with open(fname, 'w') as f:
-        print("puzzle: {}".format(fname))
+        print("puzzle: {} is created".format(fname))
         f.write(" ".join(map(str,nums)) + "\n")
         f.write("{} {}".format(len(ivs), len(evs)) + "\n")
 
@@ -42,32 +42,17 @@ def output_puzzle(basename, inp):
             f.write("{} {}".format(p[0], p[1]) + "\n")
         f.flush()
 
-def output_task_desc(basename, inp):
-    descname = basename + ".desc"
-    with open(descname, 'w') as f:
-        print("desc: {}".format(descname))
-        f.write(inp)
-        f.flush()
-
 def main():
     args = sys.argv
     if len(args) < 2:
-        print("Usage: {} <JSON file>".format(args[0]))
+        print("Usage: {} <cond file>".format(args[0]))
         exit(1)
 
     fname = args[1]
     with open(fname, 'r') as f:
-        json_str = f.read().replace('\'', '"')
+        inp = f.read()
 
-    resp = json.loads(json_str)
-
-    if fname[-5:] == ".json":
-        basename = fname[:-5]
-    else:
-        basename = fname
-
-    output_puzzle(basename, resp['puzzle'])
-    output_task_desc(basename, resp['task'])
+    output_puzzle(fname, inp)
 
 if __name__ == '__main__':
     main()
