7550cc8ac9d64048eb46e129c5acec9a3434963e
Takuto Ikuta <atetubou@gmail.com>
2019-06-23 11:16:42 +0900

    group (#179)
    * extract group
    
    * extra group

--- tailed/LKH3_wrapper.cpp
+++ tailed/LKH3_wrapper.cpp
@@ -19,9 +19,13 @@
 #include <experimental/filesystem>
 
 #include "glog/logging.h"
+#include "gflags/gflags.h"
+
+#include "base/graph.h"
+
+DEFINE_string(LKH3path, "./tailed/LKH", "");
 
 using namespace std;
-
 
 typedef vector<vector<int>> dist_matrix_t;
 
@@ -267,6 +271,126 @@ SolveShrinkedTSP(const AI& ai, int n, const std::string& path_to_LKH3) {
   return ret;
 }
 
+std::vector<std::vector<pos>> get_groups(const AI& ai,
+					 const std::vector<pos>& tsp_tours,
+					 const std::vector<pos>& want_visit) {
+  int w = ai.board.size();
+  int h = ai.board[0].size();
+
+  std::vector<std::vector<bool>> visited(w, std::vector<bool>(h));
+  std::vector<std::vector<pos>> groups(tsp_tours.size());
+
+  std::queue<std::pair<int, pos>> q;
+  for (auto i = 0u; i < tsp_tours.size(); ++i) {
+    q.push(std::make_pair(i, tsp_tours[i]));
+  }
+
+  int dx[] = {0, 0, 1, -1};
+  int dy[] = {1, -1, 0, 0};
+
+  while (!q.empty()) {
+    auto cur = q.front();
+    q.pop();
+    auto curpos = cur.second;
+    if (visited[curpos.first][curpos.second]) {
+      continue;
+    }
+    visited[curpos.first][curpos.second] = true;
+    groups[cur.first].push_back(curpos);
+
+    for (int i = 0; i < 4; ++i) {
+      int nx = curpos.first + dx[i];
+      int ny = curpos.second + dy[i];
+      if (nx < 0 || ny < 0 || nx >= w || ny >= h ||
+	  ai.board[nx][ny] == '#' || visited[nx][ny]) {
+	continue;
+      }
+      q.push({cur.first, {nx, ny}});
+    }
+  }
+
+  return groups;
+}
+
+std::vector<std::pair<int, int>> tikutaOrder(const AI& ai, int n,
+					     const std::vector<std::pair<int, int>>& want_visit) {
+  {
+    auto board = ai.board;
+    auto p = ai.get_pos();
+    board[p.first][p.second] = 'W';
+    for (auto b : board) {
+      LOG(INFO) << b;
+    }
+  }
+
+  auto tsp_tours = SolveShrinkedTSP(ai, n, FLAGS_LKH3path);
+
+  auto groups = get_groups(ai, tsp_tours, want_visit);
+
+  {
+    auto board = ai.board;
+    auto p = ai.get_pos();
+    board[p.first][p.second] = 'W';
+    for (auto i = 0u; i < groups.size(); ++i) {
+      for (const auto& g : groups[i]) {
+	auto& b = board[g.first][g.second];
+	if (b == '.') b = 128 + i;
+      }
+    }
+
+    for (const auto& row : board) {
+      for (const auto& g : row) {
+	if (g >= 0) {
+	  std::cerr << g << g;
+	} else {
+	  char buf[8];
+	  sprintf(buf, "%2d", static_cast<unsigned char>(g)-128);
+	  std::cerr << buf;
+	}
+      }
+      std::cerr << std::endl;
+    }    
+  }
+
+  GridGraph gridg(ai.board);
+
+  std::vector<pos> order;
+
+  {
+    std::vector<std::vector<bool>> visited(ai.board.size(),
+					   std::vector<bool>(ai.board[0].size()));
+    pos cur = groups[0][0];
+
+    for (auto group : groups) {
+      for(;;) {
+	order.push_back(cur);
+	visited[cur.first][cur.second] = true;
+	bool end = true;
+
+	int nearest = 1e9;
+	pos ne;
+
+	for (const auto& p : group) {
+	  if (visited[p.first][p.second]) {
+	    continue;
+	  }
+	  end = false;
+	  int nc = gridg.shortest_path(cur.first, cur.second,
+				       p.first, p.second);
+	  if (nc < nearest) {
+	    nearest = nc;
+	    ne = p;
+	  }
+	}
+
+	if (end) break;
+	cur = ne;
+      }
+    }
+  }
+
+  return order;
+}
 
 /*
 int main() {
--- tailed/LKH3_wrapper.h
+++ tailed/LKH3_wrapper.h
@@ -9,9 +9,14 @@
 #include <cstdlib>
 #include <cassert>
 
+#include "gflags/gflags.h"
+
 #include "base/ai.h"
 
 typedef std::vector<std::vector<int>> dist_matrix_t;
+using pos = std::pair<int, int>;
+
+DECLARE_string(LKH3path);
 
 std::string tempfile_name();
 
@@ -20,3 +25,6 @@ std::vector<int> SolveTSPByLKH3(const dist_matrix_t &d, const char *path_to_LKH3
 // Return tour vising n randomly selected position via LKH3.
 std::vector<std::pair<int, int>>
 SolveShrinkedTSP(const AI& ai, int n, const std::string& path_to_LKH3);
+
+std::vector<std::pair<int, int>> tikutaOrder(const AI& ai, int n,
+					     const std::vector<std::pair<int, int>>& want_visit);
