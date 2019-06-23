4442fa93582f0edf70d631d6e1123c75ddcea1a8
tikuta <tikuta@linux>
2019-06-23 11:34:23 +0900

    lkh3path

--- tailed/LKH3_wrapper.h
+++ tailed/LKH3_wrapper.h
@@ -22,9 +22,5 @@ std::string tempfile_name();
 
 std::vector<int> SolveTSPByLKH3(const dist_matrix_t &d, const char *path_to_LKH3);
 
-// Return tour vising n randomly selected position via LKH3.
-std::vector<std::pair<int, int>>
-SolveShrinkedTSP(const AI& ai, int n, const std::string& path_to_LKH3);
-
 std::vector<std::pair<int, int>> tikutaOrder(const AI& ai, int n,
 					     std::vector<std::pair<int, int>> want_visit=std::vector<std::pair<int, int>>());
