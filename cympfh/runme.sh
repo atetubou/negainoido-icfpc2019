4ae20a34e648e407a648337bc91fd54fd25cf004
/c/ympfh <cympfh@gmail.com>
2019-06-23 20:15:09 +0900

    cympfh (#218)
    * hogehogehogehgoe
    
    * hogehoge

---
+++ cympfh/runme.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TASK_ID=$( printf "%03d" $1 )
+cargo run ../problems/prob-$TASK_ID.in
+
+echo "Submitting /tmp/$TASK_ID.out"
+SUBMIT_ID=$(
+    curl -k https://negainoido.dip.jp/score/solution -F score=4 -F file=@../problems/prob-$TASK_ID.in.hand.out -F solver="$(whoami)-hand" -F task=$TASK_ID |
+        jq .solution.id
+)
+
+echo "Validating $SUBMID_ID"
+curl --fail -k https://negainoido.dip.jp/score/solution/$SUBMIT_ID/validate
