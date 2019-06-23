dac02a7da9816d78f16a0f8231374106c0724c0b
Keiichi Watanabe <udon.watanabe@gmail.com>
2019-06-23 16:19:10 +0900

    Support Clone api (#205)
    * ai.cc: Use vector for worker
    
    * Implement logics for updating states
    
    * Implement clone API

--- base/ai.h
+++ base/ai.h
@@ -14,6 +14,27 @@ enum class Direction {
   Up,
 };
 
+enum class CmdType {
+  Move = 0,
+  TurnCW,
+  TurnCCW,
+  UseExtension,
+  UseFastWheel,
+  UseDrill,
+  UseClone,
+};
+
+struct Command {
+  CmdType type;
+
+  // valid if type == "Move"
+  Direction dir;
+
+  // valid if type == "UseExtension"
+  int dx;
+  int dy;
+};
+
 char direction_to_char(Direction d);
 absl::optional<Direction> char_to_direction(char c);
 
@@ -37,15 +58,12 @@ public:
     {-1, 1},
   };
 
-  int count_fast = 0;
-  int count_drill = 0;
-  int count_extension = 0;
-
   // Remaining duration for each drill's effect.
   static const int DURATION_FAST_MAX = 50;
   static const int DURATION_DRILL_MAX = 30;
   int duration_drill = 0;
   int duration_fast = 0;
+
 };
 
 class AI {
@@ -57,16 +75,27 @@ class AI {
   int filled_count = 0;
   int block_count = 0;
 
-  bool fill_cell(Position);
+  bool fill_cell(Position, const int id = 0);
 
-  Worker worker;
+  int count_fast = 0;
+  int count_drill = 0;
+  int count_extension = 0;
+  int count_clone = 0;
 
-  void next_turn();
+  int expect_worker_id = 0;   // Rotate from 0 to (workers.size()-1)
+  std::vector<Worker> workers;
+
+  void next_turn(const int id);
   bool valid_pos(Position);
-  bool move_body(const Direction &dir);
+  bool move_body(const Direction &dir, const int id = 0);
 
 
   void initialize();
+
+  void init_turn(const int id);
+  void pickup_booster(const int id);
+
+  void push_command(struct Command cmd, const int id);
 
 public:
 
@@ -75,7 +104,7 @@ public:
 
   std::vector<std::string> board;
   std::vector<std::vector<bool>> filled;
-  std::vector<std::string> executed_cmds;
+  std::vector<std::vector<Command>> executed_cmds;
 
   int get_height();
   int get_width();
@@ -85,9 +114,10 @@ public:
   // Gets current time
   int get_time();
   // Gets the current position
-  Position get_pos() const;
+  Position get_pos(const int id = 0) const;
+
   // Gets the current direction
-  Direction get_dir();
+  Direction get_dir(const int id = 0);
   // Gets the current direction
   int get_filled_count();
 
@@ -95,34 +125,37 @@ public:
   int get_count_fast();
   int get_count_drill();
   int get_count_extension();
+  int get_count_clone();
 
   // Gets the unit time until each tool is consumed.
   // Returns 0 if the tool is not used now.
-  int get_duration_fast();
-  int get_duration_drill();
+  int get_duration_fast(const int id = 0);
+  int get_duration_drill(const int id = 0);
 
   // Returns the position that is next to the current position.
-  Position get_neighbor(const Direction &dir);
+  Position get_neighbor(const Direction &dir, const int id = 0);
 
   // Gets the all (possible) positions of manipulators.
   // It may be in invalid postion. (e.g. out of the map, unreachable postion)
-  std::vector<Position> get_absolute_manipulator_positions();
+  std::vector<Position> get_absolute_manipulator_positions(const int id = 0);
 
   // Checks if pos is 'reachable' from the current position.
-  bool reachable(Position pos);
+  bool reachable(Position pos, const int id = 0);
 
   // Checks if the robot can move to the direction.
   // This doesn't change any internal state.
-  bool try_move(const Direction &dir);
+  bool try_move(const Direction &dir, const int id = 0);
 
   // Executes a command and updates the internal state.
   // If it's a invalid move, it returns false without changing internal states.
-  bool move(const Direction &dir);
-  bool use_extension(const int dx, const int dy);
-  bool use_fast_wheel();
-  bool use_drill();
-  void turn_CW();  // Turn commands always succeed
-  void turn_CCW();
+  bool move(const Direction &dir, const int id = 0);
+  bool use_extension(const int dx, const int dy, const int id = 0);
+  void turn_CW(const int id = 0);  // Turn commands always succeed
+  void turn_CCW(const int id = 0);
+
+  bool use_fast_wheel(const int id = 0);
+  bool use_drill(const int id = 0);
+  bool use_clone(const int id);
 
   // Checks if get_filled_count() == Height * Width
   bool is_finished();
