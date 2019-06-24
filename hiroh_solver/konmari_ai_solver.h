d61943678cd71a5722b1781827aca72cba12926f
Hirokazu Honda (a.k.a hiroh) <rosetta.jpn@gmail.com>
2019-06-24 14:45:37 +0900

    Clone (#270)

---
+++ hiroh_solver/konmari_ai_solver.h
@@ -0,0 +1,26 @@
+#ifndef KONMARI_AI_SOLVER_H
+#define KONMARI_AI_SOLVER_H
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <utility>
+#include <set>
+#include <map>
+
+class KonmariAISolver {
+ public:
+  KonmariAISolver(const std::pair<int,int>& initial,
+                  const std::vector<std::string>& board,
+                  const std::vector<std::vector<bool>>& filled,
+                  const std::set<std::pair<int,int>>& area
+                  /*, std::map<char, int> extensions*/);
+  ~KonmariAISolver();
+
+  std::string solve();
+ private:
+  class Impl;
+  std::unique_ptr<Impl> impl;
+};
+
+#endif
