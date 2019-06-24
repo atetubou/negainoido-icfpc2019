20c612ebdbcedf6aa9d71f90c4cdae19c32b43ac
/c/ympfh <cympfh@gmail.com>
2019-06-24 15:09:45 +0900

    Cympfh/hand/new/buy (#272)
    * run --release
    
    * Support mouse
    
    * use B
    
    * support buy

--- cympfh/runme.sh
+++ cympfh/runme.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     cat <<EOM
-Usage: ./runme.sh 39  # <= solve the problem #039
+Usage: ./runme.sh <TASK_ID> <BUY_ITEMS>
+
+TASK_ID
+    decimal
+
+BUY_ITEMS
+    e.g.) "CCCB"
 EOM
     exit
 fi
 
 TASK_ID=$( printf "%03d" $1 )
-cargo run --release ../problems/prob-$TASK_ID.in
+cargo run --release ../problems/prob-$TASK_ID.in "$2"
+
+echo "$2" > ../problems/prob-$TASK_ID.buy
 
 echo "Submitting /tmp/$TASK_ID.out"
 SUBMIT_ID=$(
-    curl -k https://negainoido.dip.jp/score/solution -F score=4 -F file=@../problems/prob-$TASK_ID.in.hand.out -F solver="$(whoami)-hand" -F task=$TASK_ID |
+    curl -k https://negainoido.dip.jp/score/solution \
+        -F score=4 \
+        -F file=@../problems/prob-$TASK_ID.in.hand.out \
+        -F buyFile=@../problems/prob-$TASK_ID.buy \
+        -F solver="$(whoami)-hand" \
+        -F task=$TASK_ID |
         jq .solution.id
 )
 
